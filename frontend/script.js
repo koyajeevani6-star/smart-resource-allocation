f97cb14583808bde0755f7f9139c1f3649c6494b
hasini-kothamasu <hasinikkvl@gmail.com>
2026-03-18 19:53:55 +0530

    Add functions for volunteer and task management

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,44 @@
+const API = "http://127.0.0.1:5000";
 
+function addVolunteer() {
+    fetch(`${API}/add_volunteer`, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            name: document.getElementById("vname").value,
+            skill: document.getElementById("vskill").value
+        })
+    })
+    .then(() => loadData());
+}
+
+function addTask() {
+    fetch(`${API}/add_task`, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            task: document.getElementById("tname").value,
+            required_skill: document.getElementById("tskill").value,
+            priority: document.getElementById("tpriority").value
+        })
+    })
+    .then(() => loadData());
+}
+
+function autoAssign() {
+    fetch(`${API}/auto_assign`)
+        .then(res => res.json())
+        .then(data => {
+            document.getElementById("output").innerText =
+                JSON.stringify(data, null, 2);
+        });
+}
+
+function loadData() {
+    fetch(`${API}/data`)
+        .then(res => res.json())
+        .then(data => {
+            document.getElementById("output").innerText =
+                JSON.stringify(data, null, 2);
+        });
+}
